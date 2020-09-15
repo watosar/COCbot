e8b4cd833a3a9dd6713540c332fdf99aacb9f164
watosar/nekojyarasi <38435692+watosar@users.noreply.github.com>
2020-09-15 17:14:04 +0900

    Split the data storage tasks from the client.

--- CoCBot.py
+++ CoCBot.py
@@ -1,100 +1,39 @@
-# -*- coding: utf-8 -*-
- 
 from discord import Client
 import asyncio
 import os
 import json
 from cocbotcore import Commands, MultiGuildHandler, GuildCharaDataCollection
-
+from Dishelve import DiShelve
  
 class CocBotClient(Client):
-    """
-    nekojyarasi#9236 のCoC支援Bot
-    プレフィックスは有りません
-    * 機能一覧
-        + キャラクターシートのデータのキャッシュ
-            set URL
-                対応済みURL
-                - [TRPGキャラシート アーカイブス](chara.revinx.net)
-                - [キャラクター保管所](charasheet.vampire-blood.net)
-        + キャッシュされた値への補正の一時保存
-            ステータス/技能名 [-+]= 数式
-        + ニックネームでのHP/SAN/MP表示
-            (ex. nekojyarasi HP:00/00|SAN:00/00|MP:00/00)
-        + ダイスロール
-            chara   
-                キャラクターのステータス設定用のダイス
-            数式 
-                (xdyを含む場合)
-            $技能名 
-                技能値表示
-            $技能名[-+]?=[0-9]+ 
-                技能値編集
-            数式？ 
-                1d100成否ダイス
-            数式？？ 
-                シークレット1d100成否ダイス
-    """
- 
     def __init__(self):
         super().__init__()
         self.owner = None
         self._data_guild_id = None
         self.guild_handler = MultiGuildHandler()
         self.commands = Commands(client=self)
-        
-    @property
-    def data_guild(self):
-        return self.get_guild(self._data_guild_id)
+        self.dishelve = None
     
-    @data_guild.setter
-    def data_guild(self, guild):
-        self._data_guild_id = guild.id
-        
-    def get_data_guild_channel_from_guild_id(self, guild_id):
-        return next(ch for ch in self.data_guild.channels if ch.name==str(guild_id))
- 
     async def on_ready(self):
         self.owner = (await self.application_info()).owner
         print(f'Logged on as {client.user.name}!')
         print(f'owner is {self.owner}')
-        print('start setup data guild')
-        await self.setup_data_guild()
+        print('init shelve guild')
+        await self.init_dishelve()
         print('done')
         
-    async def setup_data_guild(self):
+    async def init_dishelve(self):
         for g in self.guilds:
             if g.owner == self.user:
                 break
         else:
             g = await self.create_guild(name='data')
         
-        self.data_guild = g
-        print('data guild is :', g)
-        channel_names = tuple(i.name for i in self.data_guild.channels)
-        for g in self.guilds:
-            if g == self.data_guild:
-                continue
-            if str(g.id) not in channel_names:
-                ch = await self.data_guild.create_text_channel(name=g.id)
-                await ch.send('0{}1')
+        self.dishelve = DiShelve(self, g.id)
+        await self.dishelve.ready()
     
     async def init_guild_charas_data(self, guild_id):
-        ch = self.get_data_guild_channel_from_guild_id(guild_id)
-        content = ''
-        find_end = False
-        async for msg in ch.history(limit=None):
-            if find_end:
-                if msg.content.startswith('0{'):
-                    content = msg.content[2:]+content
-                    break
-                content = msg.content+content
-            elif msg.content.endswith('}1'):
-                find_end = True
-                content = msg.content[:-1]
-                if content.startswith('0{'):
-                    content = content[1:]
-                    break
+        content = await self.dishelve.read(guild_id)
         data = json.loads(content)
         characollection = self.guild_handler.add_characollection(guild_id, {})
         if data:
@@ -104,15 +43,8 @@ class CocBotClient(Client):
         
     async def save(self, guild_id):
         print('save start')
-        ch = self.get_data_guild_channel_from_guild_id(guild_id)
         data = self.guild_handler.get_characollection(guild_id).url_dict
-        text = f'0{json.dumps(data)}1'
-        while len(text)>2000:
-            _text = text[:200]
-            await ch.send(_text)
-            text = _text
-        if text:
-            await ch.send(text)
+        self.dishelve.write(guild_id, json.dumps(data))
         
     async def on_message(self, msg):
         if msg.author == self.user:
@@ -120,11 +52,12 @@ class CocBotClient(Client):
         if debug:
             if msg.author != self.owner:
               return 
-        print('msg is :', msg.content)
+        #print('msg is :', msg.content)
         if msg.guild and ('$' in msg.content or msg.content.startswith('set ')):
             guild_id = msg.guild.id
             
-            collection = self.guild_handler.get_characollection(guild_id) or await self.init_guild_charas_data(guild_id)
+            collection = self.guild_handler.get_characollection(guild_id) \
+                            or await self.init_guild_charas_data(guild_id)
             
         else:
             collection = GuildCharaDataCollection({})
@@ -137,8 +70,7 @@ class CocBotClient(Client):
                 await msg.channel.send('success')
  
     async def on_guild_join(self, guild):
-        ch = await self.data_guild.create_text_channel(guild.id)
-        await ch.send('0{}1')
+        await self.dishelve.add_shelf(guild.id)
 
 
 debug = os.environ.get('debug',False)
---
+++ Dishelve.py
@@ -0,0 +1,63 @@
+
+import typing
+
+class DiShelve:
+    def __init__(self, client, shelve_guild_id : int):
+        self.client = client
+        self._shelve_guild_id: int = shelve_guild_id
+    
+    @property
+    def _shelve_guild(self):
+        return self.client.get_guild(self._shelve_guild_id)
+
+    async def ready(self):
+        prepared_guild_id_set = set(int(i.name) for i in self._shelve_guild.channels) 
+        for g in self.client.guilds:
+            if g.id == self._shelve_guild: continue
+            elif g.id not in prepared_guild_id_set:
+                ch = await self._shelve_guild.create_text_channel(name=g.id)
+                await ch.send("0{}1")
+    
+    def _get_channel_of_guild(self, guild_id: int):
+        key = str(guild_id)
+        return next(
+            ch for ch in self._shelve_guild.channels
+                if ch.name == key
+        )
+    
+    async def add_shelf(self, guild_id):
+        ch = await  self._shelve_guild.create_text_channel(guild_id)
+        await ch.send("0{}1")
+
+    async def write(self, guild_id: int, json_data: str):
+        ch = self._get_channel_of_guild(guild_id)
+        text = f"0{json_data}1"
+        for _ in range(len(text)//2000):
+            text_, text = text[:2000], text[2000:] 
+            await ch.send(text_)
+        if text:
+            await ch.send(text)
+
+    async def read(self, guild_id: int) -> str:
+        ch = self._get_channel_of_guild(guild_id)
+        if not ch:
+            raise ValueError(f"no available channel for the guild: {guild_id}")
+        content = ""
+        found_last_symbol = False
+        async for msg in ch.history(limit=None):
+            if not found_last_symbol:
+                if not msg.content.endswith("}1"): 
+                    raise ValueError(f"invalid style message in channle: {guild_id}")
+                    
+                found_last_symbol = True
+                content = msg.content[:-1]
+                if content.startswith('0{'):
+                    content = content[1:]
+                    break
+            elif msg.content.startswith('0{'):
+                content = msg.content[1:]+content
+                break
+            else:
+                content = msg.content+content
+        return content
+    
